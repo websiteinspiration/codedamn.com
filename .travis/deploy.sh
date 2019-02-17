b7c06f2a90aee0e2a13adbec95d2d5c55b7b7198
Mehul Mohan <mehulmpt@gmail.com>
2019-02-18 00:31:45 +0530

    Ready for production?

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 pwd
-rm ../.gitignore
-mv ../.gitignore_travis ../.gitignore
+rm .gitignore
+mv .gitignore_travis .gitignore
+rm -rf .git
+git init
+git add .
+git commit -m "Deploying"
 git config --global push.default simple
 git remote add production ssh://dev@$CODEDAMNIP/~/new-codedamn
 git push production master # push our updates
