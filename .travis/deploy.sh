20807716397d9465426647f5bc87df06b208a1fe
Mehul Mohan <mehulmpt@gmail.com>
2019-02-18 01:40:30 +0530

    Fix to bash command

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,4 +11,4 @@ git config --global push.default simple
 git remote add production ssh://dev@$CODEDAMNIP/~/new-codedamn
 git push --force production master # push our updates
 
-ssh dev@$CODEDAMNIP 'screen -S backup -d -m cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit'
+ssh dev@$CODEDAMNIP 'screen -S backup -d -m "cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit"'
