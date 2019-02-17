859a9841f75224ee7b4dbc15f104f44a47c80813
Mehul Mohan <mehulmpt@gmail.com>
2019-02-18 01:52:18 +0530

    Updated travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
 pwd
-rm .gitignore
-mv .gitignore_travis .gitignore
-rm -rf .git
-git init
-git add .
-git commit -m "Deploying"
+
 git config --global push.default simple
 git remote add production ssh://dev@$CODEDAMNIP/~/new-codedamn
-git push --force production master # push our updates
+git push production master # push our updates
 
-ssh dev@$CODEDAMNIP 'screen -S backup -d -m "cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit"'
+ssh dev@$CODEDAMNIP 'screen -S backup -d -m `cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit`'
