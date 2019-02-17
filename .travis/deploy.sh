decb1c64af7a3f4143465c7ffce9a9d6b8941c85
Mehul Mohan <mehulmpt@gmail.com>
2019-02-18 01:24:14 +0530

    Added ssh script for remote changes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,3 +10,5 @@ git commit -m "Deploying"
 git config --global push.default simple
 git remote add production ssh://dev@$CODEDAMNIP/~/new-codedamn
 git push production master # push our updates
+
+ssh dev@$CODEDAMNIP 'screen -S backup -d -m cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit'
