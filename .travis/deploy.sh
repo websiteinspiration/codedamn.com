21b8fca9b56a56930ebc6778f8ff2765618811d2
Mehul Mohan <mehulmpt@gmail.com>
2019-02-18 01:55:08 +0530

    Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,6 @@ pwd
 
 git config --global push.default simple
 git remote add production ssh://dev@$CODEDAMNIP/~/new-codedamn
-git push production master # push our updates
+git push --force production master # push our updates
 
 ssh dev@$CODEDAMNIP 'screen -S backup -d -m `cd ~/new-codedamn/ && npm install && pm2 restart codedamn.com_new && exit`'
