cb5b6cfc6282fcc000e8726dbe040dcd2f699708
Mehul Mohan <mehulmpt@gmail.com>
2019-04-06 02:52:12 +0530

    code splits

--- webpack/common.js
+++ webpack/common.js
@@ -69,8 +69,30 @@ module.exports = [
 			showErrors: true,
 			alwaysWriteToDisk: true
 		}),
-		new HtmlWebpackHarddiskPlugin()
-	]
+		new HtmlWebpackHarddiskPlugin(),
+		new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
+	],
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+		  chunks: 'all',
+		  maxInitialRequests: Infinity,
+		  minSize: 0,
+		  cacheGroups: {
+			vendor: {
+			  test: /[\\/]node_modules[\\/]/,
+			  name(module) {
+				// get the name. E.g. node_modules/packageName/not/this/part.js
+				// or node_modules/packageName
+				const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+	
+				// npm package names are URL-safe, but some servers don't like @ symbols
+				return `npm.${packageName.replace('@', '')}`;
+			  },
+			},
+		  },
+		},
+	},
 },
 // server now
 {
