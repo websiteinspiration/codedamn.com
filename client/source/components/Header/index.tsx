0752661c0d8d370848aaea808194270087a09754
Mehul Mohan <mehulmpt@gmail.com>
2019-04-06 01:53:51 +0530

    heap

--- client/source/components/Header/index.tsx
+++ client/source/components/Header/index.tsx
@@ -11,50 +11,6 @@ const mapStateToProps = ({system}) => ({
 	headerType: system.headerType
 })
 
-let rightNavLoggedOut = (
-	<div styleName="right-transparent-nav">
-		<div styleName="item">
-			<Link to="/about">Know More</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to="/feedback">Feedback</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to={{pathname: "/register", search: window.location.search}}>Register</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to={{pathname: "/login", search: window.location.search}}>Login</Link>
-		</div>
-
-	</div>
-)
-
-let rightNavLoggedIn = (
-	<div styleName="right-transparent-nav">
-		<div styleName="item">
-			<Link to={"/damn-table"}>Damn Table</Link>
-		</div>
-		<div styleName="item">
-			<Link to={"/settings"}>Settings</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to={"/feedback"}>Feedback</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to={"/about"}>Know More</Link>
-		</div>
-
-		<div styleName="item">
-			<Link to={"/logout"}>Logout</Link>
-		</div>
-	</div>
-)
-
 let loggedInLinks = (
 	<>
 		<Link styleName='item' to={"/damn-table"}>Damn Table</Link>
@@ -65,56 +21,36 @@ let loggedInLinks = (
 
 let loggedOutLinks = (
 	<>
-		<Link to={{pathname: "/login", search: window.location.search}} styleName='item'>
-			Login
-		</Link>
-		<Link to={{pathname: "/register", search: window.location.search}} styleName='item signup-button'>
-			Register
-		</Link>
+		<Link to={{pathname: "/login", search: window.location.search}} styleName='item'>Login</Link>
+		<Link to={{pathname: "/register", search: window.location.search}} styleName='item signup-button'>Register</Link>
 	</>
 )
 
 function Header(props) {
 
-	const [menuVisible, setMenuVisible] = useState(false)
-
-	function expandLinksResponsive(event) {
-		setMenuVisible(menuVisible => !menuVisible)
-        event.preventDefault()
-	}
-
 	const { userLoggedIn } = props
 	const nav = userLoggedIn? loggedInLinks: loggedOutLinks
 
-	const leftNavbarStyles = [styles['navbar-left-links']]
-	const rightNavbarStyles = [styles['navbar-right']]
-	if (!menuVisible) {
-		leftNavbarStyles.push(styles.hideOnMobile)
-		rightNavbarStyles.push(styles.hideOnMobile)
-	}
-
 	return (
-	<header styleName='main-navbar'>
-		<div styleName='navbar-left'>
+	<header styleName="main-navbar">
+		<div styleName="logo">
 			<Link to="/" styleName="image-text-pair">
 				<img src={`/assets/images/red-logo.png`} />
 
-				<div styleName='textDiv'>
+				<div styleName="text">
 					codedamn
 				</div>
+				</Link>
+		</div>
 
-				<div styleName='expand-icon'>
-					<Menu onClick={(event) => expandLinksResponsive(event)} />
-				</div>
-			</Link>
-
-			<div className={leftNavbarStyles.join(' ')}>
+		<div styleName="center-nav">
+			<div styleName="navbar-left-links">
 				<Link to="/about" styleName='item'>About us</Link>
 				<Link to="/feedback" styleName='item'>Got feedback?</Link>
 			</div>
 		</div>
 
-		<div className={rightNavbarStyles.join(' ')}>
+		<div styleName="navbar-right">
 			{nav}
 		</div>
 	</header>
