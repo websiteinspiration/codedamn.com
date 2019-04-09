3462088df686229f024c72cd5054f7469fb5ad6b
Mehul Mohan <mehulmpt@gmail.com>
2019-04-10 01:24:41 +0530

    Analytics up

--- client/source/components/index.tsx
+++ client/source/components/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store, history } from '../redux/store'
+import ReactGA from 'react-ga'
+import * as Sentry from '@sentry/browser'
+
+
+ReactGA.initialize('UA-120902510-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+history.listen((location, action) => {
+	ReactGA.set({ page: location.pathname })
+	ReactGA.pageview(location.pathname)
+})
+
+document.location.hostname !== 'localhost' && Sentry.init({
+	dsn: 'https://718860c7f084473ab5d175647d6d74f3@sentry.io/1226318'
+})
 
 ReactDOM.render(
 	<Provider store={store}>
